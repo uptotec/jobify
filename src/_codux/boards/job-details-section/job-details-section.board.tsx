65dc34439f56c9f9d8cdd7507742beb91b14923c
mahmoud ashraf <mahmoudashraf11179@gmail.com>
2023-12-08 20:36:41 +0200

    fix boards 2

--- src/_codux/boards/job-details-section/job-details-section.board.tsx
+++ src/_codux/boards/job-details-section/job-details-section.board.tsx
@@ -1,11 +1,12 @@
-import { createBoard } from '@wixc3/react-board';
-import { JobDetailsSection } from '../../../components/job-details-section/job-details-section';
+import { createBoard } from "@wixc3/react-board";
+import { JobDetailsSection } from "../../../components/job-details-section/job-details-section";
+import { fakeJobs } from "../../../jobs";
 
 export default createBoard({
-    name: 'JobDetailsSection',
-    Board: () => <JobDetailsSection />,
+    name: "JobDetailsSection",
+    Board: () => <JobDetailsSection job={fakeJobs[0]} />,
     isSnippet: true,
     environmentProps: {
-        windowWidth: 796
-    }
+        windowWidth: 796,
+    },
 });
--- src/_codux/boards/job-title-section/job-title-section.board.tsx
+++ src/_codux/boards/job-title-section/job-title-section.board.tsx
@@ -1,11 +1,12 @@
-import { createBoard } from '@wixc3/react-board';
-import { JobTitleSection } from '../../../components/job-title-section/job-title-section';
+import { createBoard } from "@wixc3/react-board";
+import { JobTitleSection } from "../../../components/job-title-section/job-title-section";
+import { fakeJobs } from "../../../jobs";
 
 export default createBoard({
-    name: 'JobTitleSection',
-    Board: () => <JobTitleSection />,
+    name: "JobTitleSection",
+    Board: () => <JobTitleSection job={fakeJobs[0]} />,
     isSnippet: true,
     environmentProps: {
-        canvasWidth: 800
-    }
+        canvasWidth: 800,
+    },
 });
